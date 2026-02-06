99ce3275f3f0d8f78a58a018bf341858a83177fe
Prince-katiyar-0 <prince.2428ece1038@kiet.edu>
2026-02-06 12:20:48 +0000

    conversion of the datatypes

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+let score = "40wer";
+console.log(typeof score);
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33"=>33
+// "31ajs"=>nan
+// true=> 1; false=>0
+
+console.log("\n");
+let isLoggedIn=1;
+let booleanisLoggedIn=Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
+// 1=>true;0=>false
+// ""=>false
+// "Solo"=>true
+
+let randoonnumber=49
+let stringNumber=String(randoonnumber)
+console.log(stringNumber,typeof stringNumber)
+
