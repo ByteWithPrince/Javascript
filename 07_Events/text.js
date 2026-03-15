1cdce4e147b47b66e0b07b3d242e971de4b56ae0
Solo <prince.14.katiyar@gmail.com>
2026-03-15 17:16:24 +0000

    done tilll here

---
+++ 07_Events/text.js
@@ -0,0 +1,18 @@
+// javascript is synchronous and single threaded language
+
+// this was all about default js 
+
+// nwo execution context :
+//     execute one line of ccode at a time 
+//     each operation wait for the last one to complete before  execution of the other 
+
+// there are two type of js code 
+//     1. Blocking code : 
+//         it block the flow of hte program 
+
+//         read file sync 
+
+//     2. Non Blocking code: 
+//         does not blovk the execution 
+
+//         Read File Async  
